32b80379859b220128d6c4155becb7ae275782aa
[lojainahmed] <abdoeltawil78@gmail.com>
2021-11-04 00:15:38 +0200

    Abdo

--- MultiQueueSimulation/Form1.Designer.cs
+++ MultiQueueSimulation/Form1.Designer.cs
@@ -42,15 +42,17 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.Interarrival_Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.probabilty = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Cumulative_probability = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Range = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
             this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Range = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cumulative_probability = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.probabilty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Interarrival_Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -58,12 +60,13 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(174, 350);
+            this.button1.Location = new System.Drawing.Point(47, 285);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 63);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Run";
+            this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -74,24 +77,25 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(257, 241);
+            this.textBox2.Location = new System.Drawing.Point(257, 134);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 24);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(257, 187);
+            this.textBox3.Location = new System.Drawing.Point(257, 191);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 24);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(257, 130);
+            this.textBox4.Location = new System.Drawing.Point(257, 245);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 24);
             this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label1
             // 
@@ -177,36 +181,18 @@
             this.button2.TabIndex = 14;
             this.button2.Text = "Add";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(1046, 350);
+            this.button3.Location = new System.Drawing.Point(947, 350);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(129, 63);
             this.button3.TabIndex = 15;
             this.button3.Text = "Add";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // Interarrival_Time
-            // 
-            this.Interarrival_Time.HeaderText = "Interarrival Time";
-            this.Interarrival_Time.Name = "Interarrival_Time";
-            // 
-            // probabilty
-            // 
-            this.probabilty.HeaderText = "probabilty";
-            this.probabilty.Name = "probabilty";
-            // 
-            // Cumulative_probability
-            // 
-            this.Cumulative_probability.HeaderText = "Cumulative probability ";
-            this.Cumulative_probability.Name = "Cumulative_probability";
-            // 
-            // Range
-            // 
-            this.Range.HeaderText = "Range";
-            this.Range.Name = "Range";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // dataGridView2
             // 
@@ -222,31 +208,75 @@
             this.dataGridView2.Size = new System.Drawing.Size(443, 254);
             this.dataGridView2.TabIndex = 16;
             // 
-            // dataGridViewTextBoxColumn1
+            // button4
             // 
-            this.dataGridViewTextBoxColumn1.HeaderText = "Service Time";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.HeaderText = "probabilty";
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.HeaderText = "Cumulative probability ";
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 410);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 63);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Run";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // dataGridViewTextBoxColumn4
             // 
             this.dataGridViewTextBoxColumn4.HeaderText = "Range";
             this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
             // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Cumulative probability ";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "probabilty";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Service Time";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // Range
+            // 
+            this.Range.HeaderText = "Range";
+            this.Range.Name = "Range";
+            // 
+            // Cumulative_probability
+            // 
+            this.Cumulative_probability.HeaderText = "Cumulative probability ";
+            this.Cumulative_probability.Name = "Cumulative_probability";
+            // 
+            // probabilty
+            // 
+            this.probabilty.HeaderText = "probabilty";
+            this.probabilty.Name = "probabilty";
+            // 
+            // Interarrival_Time
+            // 
+            this.Interarrival_Time.HeaderText = "Interarrival Time";
+            this.Interarrival_Time.Name = "Interarrival_Time";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(1188, 350);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 63);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1378, 485);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -287,15 +317,17 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Interarrival_Time;
         private System.Windows.Forms.DataGridViewTextBoxColumn probabilty;
         private System.Windows.Forms.DataGridViewTextBoxColumn Cumulative_probability;
         private System.Windows.Forms.DataGridViewTextBoxColumn Range;
-        private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- MultiQueueSimulation/Form1.cs
+++ MultiQueueSimulation/Form1.cs
@@ -14,12 +14,208 @@ namespace MultiQueueSimulation
 {
     public partial class Form1 : Form
     {
+        SimulationSystem system = new SimulationSystem();
+        TimeDistribution timed;
         public Form1()
         {
             InitializeComponent();
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            system.NumberOfServers = Convert.ToInt32(textBox1.Text);
+            system.StoppingNumber = Convert.ToInt32(textBox2.Text);
+            if(textBox3.Text == "1")
+                system.StoppingCriteria = Enums.StoppingCriteria.NumberOfCustomers;
+            else if (textBox3.Text == "2")
+                system.StoppingCriteria = Enums.StoppingCriteria.SimulationEndTime;
+            if (textBox4.Text == "1")
+                system.SelectionMethod = Enums.SelectionMethod.HighestPriority;
+            else if (textBox4.Text == "2")
+                system.SelectionMethod = Enums.SelectionMethod.Random;
+            if (textBox4.Text == "3")
+                system.SelectionMethod = Enums.SelectionMethod.LeastUtilization;
+
+            MessageBox.Show("Done ♥");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //dataGrid.Rows[rows].Cells[col].Value.ToString()      
+            //
+            for (int i =0;i<dataGridView1.Rows.Count-1;++i)
+            {
+                    timed = new TimeDistribution();
+                    timed.Time = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value);
+                    timed.Probability = Convert.ToDecimal(dataGridView1.Rows[i].Cells[1].Value);
+                    system.InterarrivalDistribution.Add(timed);
+                
+            }
+            decimal sum = 0.00m;
+            string s2;
+            for (int i = 0; i < system.InterarrivalDistribution.Count; i++)
+            {
+                s2 = " - ";
+                system.InterarrivalDistribution[i].MinRange = (int)(sum * 100) + 1;
+                s2 = system.InterarrivalDistribution[i].MinRange.ToString()+ s2;
+                sum += system.InterarrivalDistribution[i].Probability;
+                system.InterarrivalDistribution[i].CummProbability = sum;
+                dataGridView1.Rows[i].Cells[2].Value = sum.ToString() ;
+                system.InterarrivalDistribution[i].MaxRange = (int)(sum * 100);
+                s2 = s2 + system.InterarrivalDistribution[i].MaxRange.ToString();
+                dataGridView1.Rows[i].Cells[3].Value = s2;
+                Console.WriteLine(system.InterarrivalDistribution[i].CummProbability);
+                Console.WriteLine(system.InterarrivalDistribution[i].MinRange);
+                Console.WriteLine(system.InterarrivalDistribution[i].MaxRange);
+            }
+
+            MessageBox.Show("Done ♥");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Server s = new Server();
+            for(int i=0;i<dataGridView2.Rows.Count-1;++i)
+            {
+                    timed = new TimeDistribution();
+                    timed.Time = Convert.ToInt32(dataGridView2.Rows[i].Cells[0].Value.ToString());
+                    timed.Probability = Convert.ToDecimal(dataGridView2.Rows[i].Cells[1].Value.ToString());
+                    s.TimeDistribution.Add(timed);                
+            }
+            system.Servers.Add(s);
+            int siz = system.Servers.Count;
+            system.Servers[siz-1].ID =siz ;
+            decimal sum = 0.00m;
+          
+            string s1 ;
+            for (int j = 0; j < system.Servers[siz - 1].TimeDistribution.Count; j++)
+            {
+                s1 = " - ";
+                system.Servers[siz-1].TimeDistribution[j].MinRange = (int)(sum * 100) + 1;
+                s1 = system.Servers[siz - 1].TimeDistribution[j].MinRange.ToString() + s1;
+                sum += system.Servers[siz - 1].TimeDistribution[j].Probability;
+                dataGridView2.Rows[j].Cells[2].Value = sum.ToString();
+                system.Servers[siz - 1].TimeDistribution[j].CummProbability = sum;
+                system.Servers[siz - 1].TimeDistribution[j].MaxRange = (int)(sum * 100);
+                s1 = s1 + system.Servers[siz - 1].TimeDistribution[j].MaxRange.ToString();
+                dataGridView2.Rows[j].Cells[3].Value = s1;
+                Console.WriteLine(system.Servers[siz - 1].TimeDistribution[j].CummProbability);
+                Console.WriteLine(system.Servers[siz - 1].TimeDistribution[j].MinRange);
+                Console.WriteLine(system.Servers[siz - 1].TimeDistribution[j].MaxRange);
+            }
+            MessageBox.Show("Done ♥");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            decimal sumqu = 0.0m, countofq = 0.0m;
+            decimal[] numofcustomer = new decimal[system.NumberOfServers];
+            Queue<decimal> startcus = new Queue<decimal>(), endcus = new Queue<decimal>();
+            int[] endserver = new int[system.StoppingNumber];
+            for (int i = 0; i < system.StoppingNumber; i++)
+            {
+                system.SimulationTable.Add(new SimulationCase(0, 0, 0, 0, 0, 0, null, 0, 0, 0));
+                system.SimulationTable[i].CustomerNumber = i + 1;
+                system.SimulationTable[i].RandomInterArrival = new Random().Next(1, 100);
+                for (int k = 0; k < system.InterarrivalDistribution.Count; k++)
+                {
+                    if (system.SimulationTable[i].RandomInterArrival >= system.InterarrivalDistribution[k].MinRange && system.SimulationTable[i].RandomInterArrival <= system.InterarrivalDistribution[k].MaxRange)
+                        system.SimulationTable[i].InterArrival = system.InterarrivalDistribution[k].Time;
+                }
+                if (i != 0)
+                {
+                    system.SimulationTable[i].ArrivalTime = system.SimulationTable[i - 1].ArrivalTime + system.SimulationTable[i].InterArrival;
+                }
+
+
+                int min = 99999999;
+                for (int l = 0; l < system.Servers.Count; l++)
+                {
+                    if (system.SimulationTable[i].ArrivalTime >= system.Servers[l].FinishTime)
+                    {
+                        system.SimulationTable[i].AssignedServer = system.Servers[l];
+                        system.SimulationTable[i].StartTime = system.SimulationTable[i].ArrivalTime;
+                        system.SimulationTable[i].TimeInQueue = 0;
+                        break;
+
+                    }
+                    if (system.Servers[l].FinishTime < min)
+                    {
+                        min = system.Servers[l].FinishTime;
+                        system.SimulationTable[i].AssignedServer = system.Servers[l];
+                        system.SimulationTable[i].StartTime = system.SimulationTable[i].AssignedServer.FinishTime;
+                        system.SimulationTable[i].TimeInQueue = system.SimulationTable[i].AssignedServer.FinishTime - system.SimulationTable[i].ArrivalTime;
+
+                    }
+                }
+
+                system.SimulationTable[i].RandomService = new Random().Next(1, 100);
+                for (int k = 0; k < system.InterarrivalDistribution.Count; k++)
+                {
+                    if (system.SimulationTable[i].RandomService >= system.SimulationTable[i].AssignedServer.TimeDistribution[k].MinRange && system.SimulationTable[i].RandomInterArrival <= system.SimulationTable[i].AssignedServer.TimeDistribution[k].MaxRange)
+                        system.SimulationTable[i].ServiceTime = system.SimulationTable[i].AssignedServer.TimeDistribution[k].Time;
+                }
+
+                system.SimulationTable[i].EndTime = system.SimulationTable[i].StartTime + system.SimulationTable[i].ServiceTime;
+                system.SimulationTable[i].AssignedServer.FinishTime = system.SimulationTable[i].EndTime;
+                system.SimulationTable[i].AssignedServer.TotalWorkingTime += system.SimulationTable[i].ServiceTime;
+                numofcustomer[system.SimulationTable[i].AssignedServer.ID - 1] = numofcustomer[system.SimulationTable[i].AssignedServer.ID - 1] + 1;
+                sumqu += system.SimulationTable[i].TimeInQueue;
+                if (system.SimulationTable[i].TimeInQueue > 0)
+                {
+                    countofq++;
+                    startcus.Enqueue(system.SimulationTable[i].ArrivalTime);
+                    endcus.Enqueue(system.SimulationTable[i].StartTime);
+                }
+                Console.WriteLine(system.SimulationTable[i].CustomerNumber + " " + system.SimulationTable[i].AssignedServer.ID);
+
+            }
+            for (int i = 0; i < system.NumberOfServers; i++)
+            {
+                system.Servers[i].IdleProbability = (system.SimulationTable[system.StoppingNumber - 1].EndTime - system.Servers[i].TotalWorkingTime) / (decimal)system.SimulationTable[system.StoppingNumber - 1].EndTime;
+                system.Servers[i].AverageServiceTime = (system.Servers[i].TotalWorkingTime) / numofcustomer[i];
+                system.Servers[i].Utilization = (system.Servers[i].TotalWorkingTime) / (decimal)system.SimulationTable[system.StoppingNumber - 1].EndTime;
+            }
+            int count = 0;
+            while (startcus.Count != 0 && endcus.Count != 0)
+            {
+
+                if (startcus.First() < endcus.First())
+                {
+                    count++;
+                    startcus.Dequeue();
+                }
+                else
+                {
+
+                    endcus.Dequeue();
+                    count--;
+                }
+
+
+            }
+
+            system.PerformanceMeasures.AverageWaitingTime = (sumqu / system.StoppingNumber);
+            system.PerformanceMeasures.WaitingProbability = countofq / system.StoppingNumber;
+            system.PerformanceMeasures.MaxQueueLength = count;
+            string result = TestingManager.Test(system, Constants.FileNames.TestCase1);
+            MessageBox.Show(result);
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
         {
 
         }
